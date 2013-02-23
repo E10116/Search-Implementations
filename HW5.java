b90d63841b6d776b4034e43ec5e08f0162835a81
kadensotek <bobby23jones@gmail.com>
2013-02-23 05:44:17 -0500

    Added printPath() and Greedy Search algorithm

--- HW5.java
+++ HW5.java
@@ -31,10 +31,12 @@ public class HW5
                 case 1:
                         System.out.printf("Greedy Search\n");
                         System.out.printf("#############\n");
+                        greedy(Romania);
                         break;
                 case 2:
                         System.out.printf("A* Search\n");
                         System.out.printf("#########\n");
+                        aStar(Romania);
                         break;
                 case 0:
                         quit = true;
@@ -44,6 +46,62 @@ public class HW5
                         break;
             }
         }
+    }
+
+    public static void greedy(map Romania)
+    {
+        boolean done = false;
+        city current = null;
+        city lowestSLD = null;      /* most attractive heuristic city */
+        int stepNum = 0;            /* counter for steps; used to break infinite loop */
+        int numConnects = 0;
+        int i = 0;
+
+        current = Romania.Arad;
+
+        while(!done)
+        {
+            System.out.printf("Step %d: Expanding %s\n", stepNum, current.getname());
+
+            if(current.getname().equals("Bucharest"))
+            {
+                getPath(current);
+                System.out.println();
+                done = true;
+            }
+            else if(stepNum == 30)
+            {
+                System.out.printf("\nError: No solution found.\n\n");
+                done = true;
+            }
+            else
+            {
+                numConnects = current.getconnections();
+
+                for(i=0; i<numConnects; i++)
+                {
+                    System.out.printf("\t  SLD: %3d - %s\n", current.getcity(i).getSLD(), current.getcity(i).getname());
+
+                    if((lowestSLD == null) || (current.getcity(i).getSLD() < lowestSLD.getSLD()))
+                    {
+                        lowestSLD = current.getcity(i);
+                    }
+                }
+
+                lowestSLD.camefrom = current;
+                current = lowestSLD;
+                lowestSLD = null;
+                System.out.printf("\t\tGoing to %s\n", current.getname());
+                stepNum++;
+            }
+        }
+    }
+
+    public static void aStar(map Romania)
+    {
+        System.out.println("A* Search Here.");
+
+
     }
 
     public static void printMenu()
@@ -71,5 +129,27 @@ public class HW5
         }
 
         return choice;
+    }
+
+    public static void getPath(city current)
+    /* prints pathway from Arad to Bucharest by working backwards from Bucharest*/
+    {
+        boolean done = false;
+        String path = "Bucharest";
+        current = current.camefrom;    /* starts at city before Bucharest  */
+
+        while(!done)
+        {
+            if(current.getname().equals("Arad"))
+            {
+                System.out.printf("\nPath: Arad => %s\n", path);
+                done = true;
+            }
+            else
+            {
+                path = current.getname() + " => " + path;
+                current = current.camefrom;
+            }
+        }
     }
 }
