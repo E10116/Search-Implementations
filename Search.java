94a0d3439cf84fe73062e35f057211ff7807f11c
kadensotek <bobby23jones@gmail.com>
2013-03-09 20:50:17 -0500

    Fixed class name bug

--- Search.java
+++ Search.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.InputMismatchException;
 
-public class HW5
+public class Search
 {
     public static void main(String[] args)
     {
